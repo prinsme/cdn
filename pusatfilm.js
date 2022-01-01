febf4aed3a65f1c2043ea2c7c7ac76d9ee185df8
Nathan Prinsley <89361573+prinsme@users.noreply.github.com>
2022-01-02 00:26:12 +0800

    Update pusatfilm.js

--- pusatfilm.js
+++ pusatfilm.js
@@ -1,1 +1,7 @@
-
+if(document.domain == 'example.com'){
+window.location.href = 'https://prinsh.com';
+} else if(document.domain == 'example.com'){
+window.location.href = 'https://prinsh.com';
+} else if(document.domain == 'example.com'){
+window.location.href = 'https://prinsh.com';
+}
