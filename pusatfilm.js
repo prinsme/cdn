9a6fd1b88fdbae0225b77db834f97d78d3b043b5
Nathan Prinsley <89361573+prinsme@users.noreply.github.com>
2022-01-02 00:31:05 +0800

    Delete pusatfilm.js

--- pusatfilm.js
+++
@@ -1,7 +0,0 @@
-if(document.domain == 'example.com'){
-window.location.href = 'https://prinsh.com';
-} else if(document.domain == 'example.com'){
-window.location.href = 'https://prinsh.com';
-} else if(document.domain == 'example.com'){
-window.location.href = 'https://prinsh.com';
-}
