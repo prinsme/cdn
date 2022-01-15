a63cdc857cf74c6574d108687546c063b25ec4a8
Nathan Prinsley <89361573+prinsme@users.noreply.github.com>
2022-01-15 20:30:58 +0800

    Update public.js

--- public.js
+++ public.js
@@ -2,5 +2,4 @@ if(document.domain == 'nathan.prinsh.com'){
 window.location.href = 'https://prinsh.com';
 } else if(document.domain == 'unknown.com'){
 window.location.href = 'https://myweb.com';
-  
 }
