aebd52e3754f07679b00e4506b5d32a8f517f407
Nathan Prinsley <89361573+prinsme@users.noreply.github.com>
2022-01-15 20:15:08 +0800

    Update public.js

--- public.js
+++ public.js
@@ -1,5 +1,5 @@
-if(document.domain == 'unknown.com'){
-window.location.href = 'myweb.com';
+if(document.domain == 'nathan.prinsh.com'){
+window.location.href = 'prinsh.com';
 } else if(document.domain == 'unknown.com'){
 window.location.href = 'myweb.com';
 }
