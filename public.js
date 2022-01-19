3c9d8d133701bd20331c0a9f67967ae95f9c83c2
Nathan Prinsley <89361573+prinsme@users.noreply.github.com>
2022-01-20 05:03:29 +0800

    Update public.js

--- public.js
+++ public.js
@@ -1,6 +1,10 @@
 if(document.domain == 'unknown.com'){
 window.location.href = 'https://myweb.com';
+} else if(document.domain == 'cyberstudy.my.id'){
+window.location.href = 'https://prinsh.com';
+} else if(document.domain == 'unknown.com'){
+window.location.href = 'https://myweb.com';
 } else if(document.domain == 'unknown.com'){
 window.location.href = 'https://myweb.com';
 }
-//nathan.prinsh.com, shell.prinsh.com(sell)
+//nathan.prinsh.com, shell.prinsh.com(sell), cyberstudy.my.id
