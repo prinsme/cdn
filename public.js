022e39947531c5813a0b240642a61b0c3c867e7a
Nathan Prinsley <89361573+prinsme@users.noreply.github.com>
2022-03-10 13:05:59 +0800

    Update public.js

--- public.js
+++ public.js
@@ -6,5 +6,7 @@ window.location.href = 'https://myweb.com';
 window.location.href = 'https://myweb.com';
 } else if(document.domain == 'unknown.com'){
 window.location.href = 'https://myweb.com';
+} else if(document.domain == 'https://codepad.prinsh.com'){
+  alert("ini codepad.prinsh.com");
 }
-//nathan.prinsh.com, shell.prinsh.com(sell), cyberstudy.my.id
+//nathan.prinsh.com, shell.prinsh.com(sell), cyberstudy.my.id, prinsh.com/p/bot-menulis.html
