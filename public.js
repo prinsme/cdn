54ea5c11335583e4dfe3280046404156dbfcc9c0
Nathan Prinsley <89361573+prinsme@users.noreply.github.com>
2022-09-14 18:47:13 +0800

    Update public.js

--- public.js
+++ public.js
@@ -5,7 +5,7 @@ window.location.href = 'https://myweb.com';
 } else if(document.domain == 'unknown.com'){
 window.location.href = 'https://myweb.com';
 } else if(document.domain == 'axvdigital.com'){
-alert("Pliss Jangan Recode HEHEHE\nNeed Help? WA : 0895804048990\n~ Nathan Prinsley");
+alert("Pliss Jangan Recode HEHEHEZ\nNeed Help? WA : 0895804048990\n~ Nathan Prinsley");
 window.location.href = 'https://prinsh.com';
 }
 //nathan.prinsh.com, shell.prinsh.com(sell), cyberstudy.my.id, prinsh.com/p/bot-menulis.html
