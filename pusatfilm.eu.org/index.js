7354b4b3f9c97864cb24666b4b1f6e5a053072a1
Nathan Prinsley <89361573+prinsme@users.noreply.github.com>
2022-10-27 01:01:22 +0800

    Create index.js

--- pusatfilm.eu.org/index.js
+++ pusatfilm.eu.org/index.js
@@ -1,7 +1,7 @@
 if(document.domain == 'haxor.my.id'){
 window.location.href = 'https://pusatfilm.eu.org';
-} else if(document.domain == 'example.com'){
-window.location.href = 'https://pusatfilm.eu.org';
+} else if(document.domain == 'defacehtml.web.app'){
+alert("Jangan Lupa Berkunjung Ke Web Prinsh.com Ya, Kak!");
 } else if(document.domain == 'example.com'){
 window.location.href = 'https://pusatfilm.eu.org';
 }
